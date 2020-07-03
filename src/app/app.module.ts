1aaef85f29ec65c54b94ba5323ec32b3906e2eea
Muhammad Ehtesham <Shani@Muhammads-MacBook-Air.local>
2020-07-03 16:00:05 +0500

    landing page banner component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { NavbarComponent } from './Components/navbar/navbar.component';
+import { LandingPageBannerComponent } from './Components/landing-page-banner/landing-page-banner.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent],
+  declarations: [AppComponent, NavbarComponent, LandingPageBannerComponent],
   imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
   providers: [],
   bootstrap: [AppComponent],
