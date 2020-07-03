7cf609b018b6a61495a409c211c76fa349cf830f
Muhammad Ehtesham <Shani@Muhammads-MacBook-Air.local>
2020-07-03 17:39:45 +0500

    banner completed

--- src/app/Components/landing-page-banner/landing-page-banner.component.ts
+++ src/app/Components/landing-page-banner/landing-page-banner.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { faGlobeAmericas } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-landing-page-banner',
   templateUrl: './landing-page-banner.component.html',
-  styleUrls: ['./landing-page-banner.component.css']
+  styleUrls: ['./landing-page-banner.component.css'],
 })
 export class LandingPageBannerComponent implements OnInit {
+  mapIcon = faGlobeAmericas;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/Components/navbar/navbar.component.ts
+++ src/app/Components/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { faMapMarker, faGlobeAfrica } from '@fortawesome/free-solid-svg-icons';
+import { faGlobeAmericas } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-navbar',
@@ -7,7 +7,7 @@ import { faMapMarker, faGlobeAfrica } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
-  mapIcon = faGlobeAfrica;
+  mapIcon = faGlobeAmericas;
   constructor() {}
 
   ngOnInit(): void {}
