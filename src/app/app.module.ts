a4961cfb675ef1d9c8bd5cfe86d6e990ed38c4ab
Muhammad Ehtesham <Shani@muhammads-air.home>
2020-07-04 19:39:39 +0500

    milk categories component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { NavbarComponent } from './Components/navbar/navbar.component';
 import { LandingPageBannerComponent } from './Components/landing-page-banner/landing-page-banner.component';
 import { DailyDairyWorkComponent } from './Components/daily-dairy-work/daily-dairy-work.component';
+import { MilkCategoriesComponent } from './Components/milk-categories/milk-categories.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, LandingPageBannerComponent, DailyDairyWorkComponent],
+  declarations: [AppComponent, NavbarComponent, LandingPageBannerComponent, DailyDairyWorkComponent, MilkCategoriesComponent],
   imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
   providers: [],
   bootstrap: [AppComponent],
