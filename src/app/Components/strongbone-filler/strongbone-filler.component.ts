829f2e508e5f1c04e6c2e5bdc607961db75a706c
MuhammadEhtesh <32356537+MuhammadEhtesh@users.noreply.github.com>
2020-07-05 18:05:39 +0500

    Filler section & Cities section (#3)
    * strongbode message filler section
    
    * cities components added
    
    Co-authored-by: Muhammad Ehtesham <Shani@muhammads-air.home>
    Co-authored-by: Muhammad Ehtesham <Shani@192.168.10.8>

---
+++ src/app/Components/strongbone-filler/strongbone-filler.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-strongbone-filler',
+  templateUrl: './strongbone-filler.component.html',
+  styleUrls: ['./strongbone-filler.component.css']
+})
+export class StrongboneFillerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,11 @@ import { NavbarComponent } from './Components/navbar/navbar.component';
 import { LandingPageBannerComponent } from './Components/landing-page-banner/landing-page-banner.component';
 import { DailyDairyWorkComponent } from './Components/daily-dairy-work/daily-dairy-work.component';
 import { MilkCategoriesComponent } from './Components/milk-categories/milk-categories.component';
+import { StrongboneFillerComponent } from './Components/strongbone-filler/strongbone-filler.component';
+import { PakCititesComponent } from './Components/pak-citites/pak-citites.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, LandingPageBannerComponent, DailyDairyWorkComponent, MilkCategoriesComponent],
+  declarations: [AppComponent, NavbarComponent, LandingPageBannerComponent, DailyDairyWorkComponent, MilkCategoriesComponent, StrongboneFillerComponent, PakCititesComponent],
   imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
   providers: [],
   bootstrap: [AppComponent],
