26d3d7634cd829ad7c4d294535f67eb36a46d7ce
Muhammad Ehtesham <Shani@Muhammads-MacBook-Air.local>
2020-07-07 19:35:37 +0500

    home & about navigation working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './Components/home/home.component';
+import { AboutComponent } from './Components/about/about.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { NavbarComponent } from './Components/navbar/navbar.component';
-import { LandingPageBannerComponent } from './Components/landing-page-banner/landing-page-banner.component';
-import { DailyDairyWorkComponent } from './Components/daily-dairy-work/daily-dairy-work.component';
-import { MilkCategoriesComponent } from './Components/milk-categories/milk-categories.component';
-import { StrongboneFillerComponent } from './Components/strongbone-filler/strongbone-filler.component';
 import { PakCititesComponent } from './Components/pak-citites/pak-citites.component';
 import { TestimonialComponent } from './Components/testimonial/testimonial.component';
-import { AboutComponent } from './Components/about/about.component';
-import { HomeComponent } from './Components/home/home.component';
+import { MilkCategoriesComponent } from './Components/milk-categories/milk-categories.component';
+import { DailyDairyWorkComponent } from './Components/daily-dairy-work/daily-dairy-work.component';
+import { StrongboneFillerComponent } from './Components/strongbone-filler/strongbone-filler.component';
+import { LandingPageBannerComponent } from './Components/landing-page-banner/landing-page-banner.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, LandingPageBannerComponent, DailyDairyWorkComponent, MilkCategoriesComponent, StrongboneFillerComponent, PakCititesComponent, TestimonialComponent, AboutComponent, HomeComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    NavbarComponent,
+    PakCititesComponent,
+    TestimonialComponent,
+    DailyDairyWorkComponent,
+    MilkCategoriesComponent,
+    StrongboneFillerComponent,
+    LandingPageBannerComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
   providers: [],
   bootstrap: [AppComponent],
