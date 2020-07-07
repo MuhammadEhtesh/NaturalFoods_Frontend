82248fc45dd75dd1de6f15d6d8dc7cf0e476a1cf
Muhammad Ehtesham <Shani@192.168.10.8>
2020-07-07 11:35:59 +0500

    testimonial completed

--- src/app/Components/testimonial/testimonial.component.ts
+++ src/app/Components/testimonial/testimonial.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { faQuoteLeft, faHeart } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-testimonial',
   templateUrl: './testimonial.component.html',
-  styleUrls: ['./testimonial.component.css']
+  styleUrls: ['./testimonial.component.css'],
 })
 export class TestimonialComponent implements OnInit {
+  faQuoteLeft = faQuoteLeft;
+  faHeart = faHeart;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
