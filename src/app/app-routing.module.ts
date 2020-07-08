741088a3be44d0ee86ce181879df945a622c5819
Muhammad Ehtesham <Shani@muhammads-air.home>
2020-07-08 10:57:41 +0500

    signupfor routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './Components/home/home.component';
 import { AboutComponent } from './Components/about/about.component';
+import { SignupOptionsComponent } from './Components/signup-options/signup-options.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
+  { path: 'signupfor', component: SignupOptionsComponent },
 ];
 
 @NgModule({
