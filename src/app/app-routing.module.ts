40070d6210fa67285d57105c3b8b61cbca84c036
Muhammad Ehtesham <Shani@muhammads-air.home>
2020-07-08 13:13:17 +0500

    routing added for customer dairyman signup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './Components/home/home.component';
 import { AboutComponent } from './Components/about/about.component';
 import { SignupOptionsComponent } from './Components/signup-options/signup-options.component';
+import { CustomerSignupComponent } from './Components/customer-signup/customer-signup.component';
+import { DairymanSignupComponent } from './Components/dairyman-signup/dairyman-signup.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'signupfor', component: SignupOptionsComponent },
+  { path: 'customer-signup', component: CustomerSignupComponent },
+  { path: 'dairyman-signup', component: DairymanSignupComponent },
 ];
 
 @NgModule({
